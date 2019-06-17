110b7ab41aab6e3d99310f822ee4a1f54547e61b
Segolene ALQUIER <salquier@e3r8p12.42.fr>
2019-06-17 17:03:53 +0200

    created config files + nav

---
+++ navigation.php
@@ -0,0 +1,19 @@
+<div class="nav-wrap">
+	<div class="home-link">
+		<div><a href="/index.php"><i class="fas fa-home"></i></a></div>
+	</div>
+	<div class="log-section">
+		<?php
+			if (!$_SESSION['login'])
+			{
+				echo '<div class="log-elem"><a href="/users/login.html">Log in</a>';
+				echo '<div class="log-elem"><a href="/users/create_user.html">Sign up</a></div>';
+			}
+			else
+			{
+				echo '<div class="log-elem"><a href="/users/myaccount.php"><i class="fas fa-user"></i></a></div>';
+				echo '<div class="log-elem"><a href="/users/logout.php">Log out</a></div>';
+			}
+		?>
+	</div>
+</div>

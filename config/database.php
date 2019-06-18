4b6d7437ea2442418539117b83c9080fb7ac566d
Segolene ALQUIER <salquier@e3r6p16.42.fr>
2019-06-18 15:10:50 +0200

    create user working - need to check the email content

--- config/database.php
+++ config/database.php
@@ -4,8 +4,9 @@
 	$DB_PASSWORD = 'Rootmysql';
 	try {
 		$bdd = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
+		$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	}
-	catch (Exception $e)
+	catch (PDOException $e)
 	{
 		die('Erreur : ' . $e->getMessage());
 	}

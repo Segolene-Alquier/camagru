4af9b552068a7cdd9edbedc230206dcce3c1bc45
Segolene ALQUIER <segolene.alquier@yahoo.com>
2019-07-24 15:40:10 +0200

    added filters images + created layout editing page

---
+++ editing/edit.php
@@ -0,0 +1,42 @@
+<?php
+
+// creer restriction login
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Edit picture</title>
+    <link rel="stylesheet" type="text/css" href="../camagru.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.2/css/bulma.min.css">
+    <script src="https://kit.fontawesome.com/82e513fc69.js"></script>
+</head>
+<body>
+    <?php include_once '../navigation.php'; ?>
+	<div class="section">
+		<div class="edit-wrapper">
+			<div class="columns">
+				<div class="column is-four-fifths edit-left">
+					<div>
+
+					</div>
+					<div class="box">
+
+					</div>
+				</div>
+				<div class="column edit-right">
+					<div class="box edit-right-wrapper">
+						<img src="../img/cliff.jpg" alt="">
+						<img src="../img/cliff.jpg" alt="">
+						<img src="../img/cliff.jpg" alt="">
+
+					</div>
+				</div>
+			</div>
+		</div>
+    </div>
+</body>
+</html>

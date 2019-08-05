9d2c32a674fbf6e52be99c8c8848b0beef191042
Segolene ALQUIER <segolene.alquier@yahoo.com>
2019-08-05 18:19:01 +0200

    fixed navbar (icons + home redirection)

--- navigation.php
+++ navigation.php
@@ -1,6 +1,11 @@
 <nav class="navbar is-fixed-top is-primary" role="navigation" aria-label="main navigation">
 	<div class="navbar-brand">
-		<a href="#" class="navbar-item"><i class="fas fa-home"></i></a>
+		<?php
+			if (file_exists('index.php'))
+				echo '<a href="index.php" class="navbar-item"><i class="fas fa-home"></i></a>';
+			else
+				echo '<a href="../index.php" class="navbar-item"><i class="fas fa-home"></i></a>';
+		?>
 		<a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" onclick="document.querySelector('.navbar-menu').classList.toggle('is-active');">
 			<span aria-hidden="true"></span>
 			<span aria-hidden="true"></span>
@@ -13,11 +18,12 @@
 		</div>
 		<div class="navbar-end">
 			<?php
-			// echo $_SESSION['username'];
 			if(isset($_SESSION['username']) === true){
-				echo '<div class="navbar-item"><a href="/users/myaccount.php"><i class="fas fa-user"></i></a></div>';
-				echo '<div class="navbar-item"><a href="/camagru/users/logout.php">Log out</a></div>';
-			 } else {
+				echo '<div class="navbar-item"><a href="/camagru/editing/edit.php" title="Montage"><i class="fas fa-camera"></i></a></div>';
+				echo '<div class="navbar-item"><a href="/users/myaccount.php" title="Mon Compte"><i class="fas fa-user"></i></a></div>';
+				echo '<div class="navbar-item"><a href="/camagru/users/logout.php title="Log Out"><i class="fas fa-sign-out-alt"></i></a></div>';
+			 }
+			 else {
 				echo '<div class="navbar-item"><a href="/camagru/users/login.php">Log in</a>';
 				echo '<div class="navbar-item"><a href="/camagru/users/create_user.php">Sign up</a></div>';
 			 }

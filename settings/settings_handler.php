3eb7afb94ec366b2100ebab52cb0e5d38cf1cf21
Segolene ALQUIER <segolene.alquier@yahoo.com>
2019-09-18 14:29:58 +0200

    modify email form working

--- settings/settings_handler.php
+++ settings/settings_handler.php
@@ -3,12 +3,12 @@ session_start();
 require "settings_class.php";
 
 $user = new Setting;
-echo json_encode($_POST['newmail']);
-echo json_encode($_POST['oldmail']);
+// echo json_encode($_POST['newmail']);
+// echo json_encode($_POST['oldmail']);
 
 if (isset($_POST['newmail']) && isset($_POST['oldmail'])) {
-	// $user->modifyMail($_SESSION['username'], $_POST['oldmail'], $_POST['newmail']);
-	echo json_encode("coucou");
+	$user->modifyMail($_SESSION['username'], $_POST['oldmail'], $_POST['newmail']);
+	// echo json_encode("coucou");
 }
 // elseif (isset($_POST['clic'])) {
 // 	$boolean = $_POST['clic'];

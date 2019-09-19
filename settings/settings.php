02098090445a40f1bd29b289be720ae17e381542
Segolene ALQUIER <segolene.alquier@yahoo.com>
2019-09-19 18:00:20 +0200

    fixed design of setting page, now fully responsive

--- settings/settings.php
+++ settings/settings.php
@@ -33,100 +33,98 @@ if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST["modifyMail"]))
 </head>
 <body onload="wantsNotification()">
 	<?php include_once '../navigation.php'; ?>
-	<div class="section ">
-		<h1 class="title is-2 settings-title">Welcome to your page <?= $_SESSION['username']?>!</h1>
-		<div class="settings-wrapper">
-			<div class="box settings-div">
-				<h2 class="subtitle is-4">👋 Modify your username</h2>
-					<div class="field is-horizontal ">
-						<div class="field-body">
-							<div class="field settings-field">
-								<p class="control is-expanded has-icons-left">
-									<input required class="input" id="old_name" type="text" placeholder="Current username">
-									<span class="icon is-small is-left">
-									<i class="fas fa-user"></i>
-									</span>
-								</p>
-							</div>
-							<div class="field settings-field">
-								<p class="control is-expanded has-icons-left has-icons-right">
-									<input required class="input " id="new_name" type="text" placeholder="New username" value="">
-									<span class="icon is-small is-left">
-									<i class="fas fa-user"></i>
-									</span>
-								</p>
-							</div>
-							<div class="field">
-								<div class="control">
-									<input type="submit" name="modifyUsername" class="button is-primary" value="Save" onclick="modifyName();"/>
-								</div>
-							</div>
-						</div>
-					</div>
-			</div>
-			<div class="box settings-div">
-				<h2 class="subtitle is-4">🔑 Modify your password</h2>
-					<div class="field is-horizontal ">
-						<div class="field-body">
-							<div class="field settings-field">
-								<p class="control is-expanded has-icons-left">
-									<input required class="input" id="old_pwd" name="old_password" type="password" placeholder="Current password">
-									<span class="icon is-small is-left">
-									<i class="fas fa-key"></i>
-									</span>
-								</p>
-							</div>
-							<div class="field settings-field">
-								<p class="control is-expanded has-icons-left has-icons-right">
-									<input required class="input" id="new_pwd" name="new_password" type="password" placeholder="New password" value="">
-									<span class="icon is-small is-left">
-									<i class="fas fa-key"></i>
-									</span>
-								</p>
-							</div>
-							<div class="field">
-								<div class="control">
-									<input type="submit" name="modifyPwd" class="button is-primary" value="Save" onclick="checkPassword();"/>
-								</div>
-							</div>
-						</div>
-					</div>
-			</div>
-			<div class="box settings-div">
-				<h2 class="subtitle is-4">💌 Modify your email address</h2>
+	<div class="settings-wrapper">
+		<h1 class="title is-3 settings-title">Welcome to your page <?= $_SESSION['username']?>!</h1>
+		<div class="box settings-div">
+			<h2 class="subtitle is-4">👋 Modify your username</h2>
 				<div class="field is-horizontal ">
 					<div class="field-body">
 						<div class="field settings-field">
 							<p class="control is-expanded has-icons-left">
-								<input required class="input" id="old_mail" name="old_mail" type="text" placeholder="Current email">
+								<input required class="input" id="old_name" type="text" placeholder="Current username">
 								<span class="icon is-small is-left">
-								<i class="fas fa-envelope"></i>
+								<i class="fas fa-user"></i>
 								</span>
 							</p>
 						</div>
 						<div class="field settings-field">
 							<p class="control is-expanded has-icons-left has-icons-right">
-								<input required class="input" id="new_mail" name="new_mail" type="email" placeholder="New email" value="">
+								<input required class="input " id="new_name" type="text" placeholder="New username" value="">
 								<span class="icon is-small is-left">
-								<i class="fas fa-envelope"></i>
+								<i class="fas fa-user"></i>
 								</span>
 							</p>
 						</div>
 						<div class="field">
 							<div class="control">
-							<input type="submit" name="modifyMail" class="button is-primary" value="Save" onclick="modifyMail();"/>
+								<input type="submit" name="modifyUsername" class="button is-primary" value="Save" onclick="modifyName();"/>
 							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-			<div class="box settings-div">
-				<h2 class="subtitle is-4">🔔 Your notification preferences</h2>
-				<h3 class="subtitle is-6">Do you want to get a notification email if someone comments one of your pictures?</h3>
-				<div class="buttons has-addons is-right">
-					<span id="notif-yes" class="button is-success" onclick="notifYes();">Yes</span>
-					<span id="notif-no" class="button is-danger" onclick="notifNo();">No</span>
+		</div>
+		<div class="box settings-div">
+			<h2 class="subtitle is-4">🔑 Modify your password</h2>
+				<div class="field is-horizontal ">
+					<div class="field-body">
+						<div class="field settings-field">
+							<p class="control is-expanded has-icons-left">
+								<input required class="input" id="old_pwd" name="old_password" type="password" placeholder="Current password">
+								<span class="icon is-small is-left">
+								<i class="fas fa-key"></i>
+								</span>
+							</p>
+						</div>
+						<div class="field settings-field">
+							<p class="control is-expanded has-icons-left has-icons-right">
+								<input required class="input" id="new_pwd" name="new_password" type="password" placeholder="New password" value="">
+								<span class="icon is-small is-left">
+								<i class="fas fa-key"></i>
+								</span>
+							</p>
+						</div>
+						<div class="field">
+							<div class="control">
+								<input type="submit" name="modifyPwd" class="button is-primary" value="Save" onclick="checkPassword();"/>
+							</div>
+						</div>
+					</div>
 				</div>
+		</div>
+		<div class="box settings-div">
+			<h2 class="subtitle is-4">💌 Modify your email address</h2>
+			<div class="field is-horizontal ">
+				<div class="field-body">
+					<div class="field settings-field">
+						<p class="control is-expanded has-icons-left">
+							<input required class="input" id="old_mail" name="old_mail" type="text" placeholder="Current email">
+							<span class="icon is-small is-left">
+							<i class="fas fa-envelope"></i>
+							</span>
+						</p>
+					</div>
+					<div class="field settings-field">
+						<p class="control is-expanded has-icons-left has-icons-right">
+							<input required class="input" id="new_mail" name="new_mail" type="email" placeholder="New email" value="">
+							<span class="icon is-small is-left">
+							<i class="fas fa-envelope"></i>
+							</span>
+						</p>
+					</div>
+					<div class="field">
+						<div class="control">
+						<input type="submit" name="modifyMail" class="button is-primary" value="Save" onclick="modifyMail();"/>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="box settings-div">
+			<h2 class="subtitle is-4">🔔 Your notification preferences</h2>
+			<h3 class="subtitle is-6">Do you want to get a notification email if someone comments one of your pictures?</h3>
+			<div class="buttons has-addons is-right">
+				<span id="notif-yes" class="button is-success" onclick="notifYes();">Yes</span>
+				<span id="notif-no" class="button is-danger" onclick="notifNo();">No</span>
 			</div>
 		</div>
 	</div>

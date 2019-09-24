43167b446de0eb173d5d78d8c87bc6463b9d5700
Segolene ALQUIER <segolene.alquier@yahoo.com>
2019-09-24 15:55:28 +0200

    fixed design of reset_pwd_pending form

--- users/reset_pwd_pending.php
+++ users/reset_pwd_pending.php
@@ -20,38 +20,33 @@
     <script src="https://kit.fontawesome.com/82e513fc69.js"></script></head>
 <body>
     <?php include_once '../navigation.php'; ?>
-    <div class="block">
-        <div class="container ">
-            <div class="columns">
-                <div class="column is-half form-wrapper">
-                    <div class="box card-title">
-                        <h2 class="form-title title">Reset your password</h2>
+    <div class="container form-container">
+        <div class="column is-half form-wrapper">
+            <div class="box card-title">
+                <h2 class="form-title title">Reset your password</h2>
+            </div>
+            <div class="box">
+                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" class="control">
+                    <div class="field <?php echo (!empty($user->email_err)) ? 'has-error' : ''; ?>">
+                        <label class="label">Email</label>
+                        <div class="control has-icons-left has-icons-right">
+                            <input required type="text" name="email" class="input" value="">
+                            <span class="icon is-small is-left">
+                            <i class="fas fa-envelope"></i>
+                            </span>
+                        </div>
+                        <?php if (isset($user)) {?>
+                        <span class="help-block"><?php echo $user->email_err; ?></span>
+                        <?php }; ?>
                     </div>
-                    <div class="box">
-                        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" class="control">
-                            <div class="field <?php echo (!empty($user->email_err)) ? 'has-error' : ''; ?>">
-                                <label class="label">Email</label>
-                                <div class="control has-icons-left has-icons-right">
-                                    <input required type="text" name="email" class="input" value="">
-                                    <span class="icon is-small is-left">
-                                    <i class="fas fa-envelope"></i>
-                                    </span>
-                                </div>
-                                <?php if (isset($user)) {?>
-                                <span class="help-block"><?php echo $user->email_err; ?></span>
-                                <?php }; ?>
-                            </div>
-                            <div class="field">
-                                <input type="submit" class="button is-link" value="Submit">
-                            </div>
-                            <p>Changed your mind? <a href="login.php">Login here</a>.</p>
-                            <?php if (isset($user) && !isset($user->email_err) && $_SERVER["REQUEST_METHOD"] == "POST") {?>
-                            <span class="help-block"><?php echo "Please check your email box, we sent you the link to reset your password"; ?></span>
-                            <?php }; ?>
-                        </form>
-
+                    <div class="field">
+                        <input type="submit" class="button is-link" value="Submit">
                     </div>
-                </div>
+                    <p>Changed your mind? <a href="login.php">Login here</a>.</p>
+                    <?php if (isset($user) && !isset($user->email_err) && $_SERVER["REQUEST_METHOD"] == "POST") {?>
+                    <span class="help-block"><?php echo "Please check your email box, we sent you the link to reset your password"; ?></span>
+                    <?php }; ?>
+                </form>
             </div>
         </div>
     </div>
